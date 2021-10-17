e8e74fc7629a1f69ba96da37c1956db0e2bc19f0
Yuri Cavalcante <matosf.yuri@gmail.com>
2021-10-17 14:28:52 -0300

    Novas alterações

--- Components/Emprestar/index.tsx
+++ Components/Emprestar/index.tsx
@@ -1,47 +1,129 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
+  TouchableWithoutFeedback,
   Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  TextInput,
   useColorScheme,
-  View
+  View,
+  Platform,
+  Button,
+  Keyboard,
+  ScrollView,
+  Image,
+  TouchableOpacity
 } from 'react-native';
-
 import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
+import { searchBooks } from '../../src/Apis';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
 
 
-const App = () => {
+function MyStack() {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Buscar" component={App} />
+      <Stack.Screen name="Detalhes" component={DetalheLivro} />
+    </Stack.Navigator>
+  );
+}
+
+const App = (props:any) => {
   const isDarkMode = useColorScheme() === 'dark';
+  const navigation = useNavigation();
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const [searchData, setSearch] = useState('');
+
+  function handleInput(event:any){
+      setSearch(event);
+  }
+
   return (
-      <SafeAreaView style={[backgroundStyle, styles.safeContainer]}>
-            <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-            <View style={[backgroundStyle, styles.container]}>
-              <Text style={[{color: isDarkMode ? Colors.white : Colors.black }]}>
-                Olá
-              </Text>
-            </View>
-          </SafeAreaView>
+    <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.container}
+    >
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={[backgroundStyle, styles.inner]}>
+        <Text style={[{color: isDarkMode ? Colors.white : Colors.black },styles.header]}>Buscar Livro</Text>
+        <TextInput onChangeText={handleInput} placeholder="Título do Livro, Autor, Editora..." style={[{color: isDarkMode ? Colors.white : Colors.black}, styles.textInput]} placeholderTextColor={isDarkMode ? Colors.white : Colors.black}/>
+        <View style={styles.btnContainer}>
+          <Button title="Pesquisar" onPress={(props:any) => navigation.navigate('Detalhes' as never, {name: searchData} as never)} />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  </KeyboardAvoidingView>
   );
 };
 
-const styles = StyleSheet.create({
-    safeContainer: {
-      flex: 1,
-      backgroundColor: 'red'
-    },
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-})
+const DetalheLivro = (props:any) =>{
 
-export default App;
+    const navigation = useNavigation();
+    console.log(props.route.params);
+    const [books, setBooks] = useState([]) as any;
+
+    async function search(){
+        let value = await searchBooks(props.route.params.name);
+        setBooks(value.items)
+      }
+
+    useEffect(() => {
+        search();
+    },[]);
+
+    const isDarkMode = useColorScheme() === 'dark';
+
+    const backgroundStyle = {
+      backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
+
+    return (
+        <SafeAreaView>
+        <ScrollView>
+                {books.map((item:any, index:number) => (
+                    <TouchableOpacity  key={index} style={{padding: 30}}>
+                        <View>
+                            <Image style={{height: 350, maxWidth: '80%'}} source={{uri: item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.thumbnail.replace("http", "https") : 'https://m.media-amazon.com/images/I/51lwu3FTjGL.jpg'}}/>
+                            <Text>{item.volumeInfo.title}</Text>
+                        </View>
+                    </TouchableOpacity>
+                ))}
+            </ScrollView>
+    </SafeAreaView>
+    )}
+
+    const styles = StyleSheet.create({
+        container: {
+          flex: 1
+        },
+        inner: {
+          padding: 24,
+          flex: 1,
+          justifyContent: "space-around"
+        },
+        header: {
+          fontSize: 36,
+          marginBottom: 48
+        },
+        textInput: {
+          height: 40,
+          borderColor: "#000000",
+          borderBottomWidth: 1,
+          marginBottom: 36
+        },
+        btnContainer: {
+          marginTop: 12
+        }
+      });
+
+export default MyStack;
--- src/Apis.ts
+++ src/Apis.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { API_BASEURL, API_LOGIN, API_PASSWORD } from 'react-native-dotenv'
+import { API_BASE, API_BASEURL, API_LOGIN, API_PASSWORD } from 'react-native-dotenv'
 
 
 export async function postUserAPI(data:any){
@@ -22,7 +22,12 @@ export async function postUserAPI(data:any){
 }
 
 export async function getUserApi(data:any){
-    axios.get(`${API_BASEURL}/usuario/${data}`)
+   return axios.get(`${API_BASEURL}/usuariostatus/${data}`, {
+      auth:{
+        "username": `${API_LOGIN}`,
+        "password": `${API_PASSWORD}`
+      }
+    })
     .then((response) => response.data)
     .catch(function (error) {
         if (error.response) {
@@ -59,5 +64,39 @@ export async function getLivros(){
         console.log(error.response.status);
         console.log(error.response.headers);
     }
+  })
+}
+
+export async function getLivrosProx(lat:number, long:number, dist:number, id: number){
+  let dados = {"dist": 3, "lat": -12.711107, "long": -38.343671, "idusuario":id}
+  console.log(dados);
+  return axios.post(`${API_BASEURL}/livrosprox`,dados, {
+    auth:{
+      "username": `${API_LOGIN}`,
+      "password": `${API_PASSWORD}`
+    }
+  }).then((response) => response.data)
+  
+  .catch((error) => {
+    if(error.response){
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+    }
+  })
+}
+
+export async function searchBooks(search:string){
+  return axios.get('https://www.googleapis.com/books/v1/volumes',{
+    params: {
+      "key": `${API_BASE}`,
+      "q":search
+    }
+  }).then((response) => response.data)
+  .catch((error) => {
+    if(error.response)
+      console.log(error.response)
+    else
+      console.log(error);
   })
 }
